a159d1d4c2ef6be5b2915dc376137f568ac6c475
Kevin OBrien <kevin.obrien@praece.com>
2015-02-07 20:58:56 -0800

    Added a migration for a user table.

--- db/migrations/20150207204939_test.js
+++ db/migrations/20150207204939_test.js
@@ -1,9 +1,14 @@
 'use strict';
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('user', function (table) {
+  	table.increments();
+	  table.datetime('createdAt');
+	  table.datetime('updatedAt');
+	  table.text('email');
+	})
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('user');
 };
